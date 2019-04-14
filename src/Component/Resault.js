7bf0889325e1382b459ca36470d9495d274c8d2a
Sergey Kurilov <sergeyksi94@mail.ru>
2019-04-14 19:49:24 +0400

    first commit

---
+++ src/Component/Resault.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import s from './Tiket.module.css'
+
+
+class Resault extends Component {
+  render() {
+    return (
+      <div>
+         123
+      </div>
+    );
+  }
+}
+
+export default Resault;
